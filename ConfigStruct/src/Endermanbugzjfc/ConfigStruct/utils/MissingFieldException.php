382d288a23789430aa7b7a215a4d19dd1846bafc
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 06:02:20 +0800

    Added properties and getters for field and config file

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/utils/MissingFieldException.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/utils/MissingFieldException.php
@@ -3,8 +3,33 @@
 namespace Endermanbugzjfc\ConfigStruct\utils;
 
 use Exception;
+use ReflectionProperty;
 
 class MissingFieldException extends Exception
 {
+
+    public function __construct(
+        protected ReflectionProperty $field,
+        protected ?string            $configFile = null
+    )
+    {
+    }
+
+    /**
+     * @return ReflectionProperty
+     */
+    public function getField() : ReflectionProperty
+    {
+        return $this->field;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getConfigFile() : ?string
+    {
+        return $this->configFile;
+    }
+
 
 }
