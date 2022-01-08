f99132f67286d347abc3682d2b56ac4554a0464a
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 03:32:00 +0800

    Added ConfigStructTest.php

---
+++ tests/ConfigStructTest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class ConfigStructTest extends TestCase
+{
+
+    public function testParseArray()
+    {
+
+    }
+
+    public function testEmit()
+    {
+
+    }
+
+    public function testEmitArray()
+    {
+
+    }
+
+    public function testParse()
+    {
+
+    }
+}
