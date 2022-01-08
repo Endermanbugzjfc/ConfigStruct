50676e07f8d1df84e70e587b3893aa721294934f
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 05:52:19 +0800

    Revert "Added throw exception as handler for missing required field"
    This reverts commit 8138f68853a70ec1002edcce180d3cc0d0957a1c.

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Required.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Required.php
@@ -3,26 +3,12 @@
 namespace Endermanbugzjfc\ConfigStruct\attributes;
 
 use Attribute;
-use Endermanbugzjfc\ConfigStruct\exceptions\SetupException;
 
 #[Attribute(Attribute::TARGET_PROPERTY)] class Required
 {
 
-    public const THROW_EXCEPTION = [self::class, "throwException"];
-
     public function __construct(callable $onMissing)
     {
-    }
-
-    /**
-     * @throws SetupException
-     */
-    public static function throwException(string $field, ?string $file = null) : void
-    {
-        throw new SetupException(
-            "Required field \"$field\" is missing"
-            . (isset($file) ? " in $file" : "")
-        );
     }
 
 }
