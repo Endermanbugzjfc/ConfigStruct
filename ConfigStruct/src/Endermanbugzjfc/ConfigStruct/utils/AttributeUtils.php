5675aba112de8119825ba35932fb25154638de57
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 02:21:18 +0800

    Added trueIfNo() in AttributeUtils.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/utils/AttributeUtils.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/utils/AttributeUtils.php
@@ -2,11 +2,27 @@
 
 namespace Endermanbugzjfc\ConfigStruct\utils;
 
+use ReflectionProperty;
+
 final class AttributeUtils
 {
 
     private function __construct()
     {
+    }
+
+    public static function trueIfNo(
+        ReflectionProperty $property,
+        string             $attribute,
+                           &$value = null
+    ) : bool
+    {
+        $attrib = $property->getAttributes($attribute)[0] ?? null;
+        if (!isset($attrib)) {
+            return false;
+        }
+        $value = $attrib;
+        return true;
     }
 
 }
