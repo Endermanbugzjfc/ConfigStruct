395d13b38733b157092c30d8c2383db157c63278
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 02:47:06 +0800

    Revert "Added emitGroup() in Group.php"
    This reverts commit 4c5c67235836410f59f3e0f874936196769d1bcc.

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Group.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Group.php
@@ -4,10 +4,6 @@ namespace Endermanbugzjfc\ConfigStruct\attributes;
 
 
 use Attribute;
-use Endermanbugzjfc\ConfigStruct\utils\AttributeUtils;
-use ReflectionAttribute;
-use ReflectionClass;
-use ReflectionProperty;
 
 #[Attribute(Attribute::TARGET_PROPERTY)] class Group
 {
@@ -18,35 +14,5 @@ use ReflectionProperty;
     public function __construct(?string $class)
     {
     }
-
-    public static function emitGroup(
-        object             $struct,
-                           &$value,
-        ReflectionProperty $property
-    ) : bool
-    {
-        if (AttributeUtils::trueIfNo(
-            $property,
-            self::class,
-            $attribute
-        )) {
-            return false;
-        }
-        assert($attribute instanceof ReflectionAttribute);
-
-        $child = $property->getValue($struct);
-        $value = (array)$child;
-        foreach (
-            (new ReflectionClass($child))
-                ->getProperties()
-            as $sProperty
-        ) {
-            if (self::emitGroup($child, $sValue, $sProperty)) {
-                $value[$sProperty->getName()] = $sValue;
-            }
-        }
-        return true;
-    }
-
 
 }
