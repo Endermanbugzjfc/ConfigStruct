b6f9c622a1decbe58ae4d4ef9dde16cf870dbb2d
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 05:49:19 +0800

    Added log as handler for missing required field

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Required.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Required.php
@@ -4,25 +4,59 @@ namespace Endermanbugzjfc\ConfigStruct\attributes;
 
 use Attribute;
 use Endermanbugzjfc\ConfigStruct\exceptions\SetupException;
+use LogLevel;
+use pocketmine\Server;
 
 #[Attribute(Attribute::TARGET_PROPERTY)] class Required
 {
 
     public const THROW_EXCEPTION = [self::class, "throwException"];
+    public const LOG = [self::class, "log"];
 
-    public function __construct(callable $onMissing)
+    public function __construct(
+        callable $onMissing,
+        ?string  $plugin = null,
+        bool     $soft = false,
+        string   $logLevel = LogLevel::ERROR
+    )
     {
     }
 
     /**
      * @throws SetupException
      */
-    public static function throwException(string $field, ?string $file = null) : void
+    public static function throwException(
+        string  $field,
+        ?string $file,
+        ?string $plugin,
+        bool    $soft,
+        string  $logLevel
+    ) : void
     {
+        // Plugin can can be seen from the stacktrace, so it is not included here.
         throw new SetupException(
             "Required field \"$field\" is missing"
             . (isset($file) ? " in $file" : "")
         );
     }
+
+    public static function log(
+        string  $field,
+        ?string $file,
+        ?string $plugin,
+        bool    $soft,
+        string  $logLevel
+    ) : void
+    {
+        Server::getInstance()
+            ->getPluginManager()
+            ->getPlugin($plugin)
+            ->getLogger()
+            ->log(
+                $logLevel, "Required field \"$field\" is missing"
+                . (isset($file) ? " in $file" : "")
+            );
+    }
+
 
 }
