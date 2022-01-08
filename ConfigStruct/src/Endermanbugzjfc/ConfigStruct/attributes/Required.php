ee4a97a365e1987b459f75e8ef73684319af1764
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 06:15:30 +0800

    Added isFieldMissing() in Required.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Required.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Required.php
@@ -3,12 +3,37 @@
 namespace Endermanbugzjfc\ConfigStruct\attributes;
 
 use Attribute;
+use Endermanbugzjfc\ConfigStruct\utils\AttributeUtils;
+use ReflectionAttribute;
+use ReflectionProperty;
+use function assert;
 
 #[Attribute(Attribute::TARGET_PROPERTY)] class Required
 {
 
     public function __construct(callable $onMissing)
     {
+    }
+
+    public static function isFieldMissing(
+        object             $struct,
+        ReflectionProperty $property
+    ) : bool
+    {
+        if (AttributeUtils::trueIfNoInProperty(
+            $property,
+            self::class,
+            $attribute
+        )) {
+            return false;
+        }
+        assert($attribute instanceof ReflectionAttribute);
+
+        $n = $property->getValue();
+        if (isset($struct->$n)) {
+            return false;
+        }
+        return true;
     }
 
 }
