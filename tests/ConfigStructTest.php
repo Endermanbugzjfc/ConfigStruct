4436e4ac81a27923c3212e5c8ac9457cc6fcfaf1
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 19:04:56 +0800

    Bro I don't to wait so much time on making unit tests

--- tests/ConfigStructTest.php
+++ tests/ConfigStructTest.php
@@ -11,56 +11,9 @@ class ConfigStructTest extends TestCase
 
     }
 
-    public function structKeyNameAttributeProvider() : object
-    {
-
-    }
-
-    public function structGroupAttributeProvider() : object
-    {
-
-    }
-
-    public function structAutoInitializeChildStructAttributeProvider() : object
-    {
-
-    }
-
-    public function structRequiredProvider() : object
-    {
-
-    }
-
-    public function structMixedAttributeProvider() : object
-    {
-
-    }
-
     public function testParseArray()
     {
 
-    }
-
-    public function testParseArrayKeyNameAttribute()
-    {
-
-    }
-
-    public function testParseArrayGroupAttribute()
-    {
-
-    }
-
-    public function testParseArrayAutoInitializeChildStructAttribute()
-    {
-    }
-
-    public function testParseArrayRequiredAttribute()
-    {
-    }
-
-    public function testParseArrayMixedAttributes()
-    {
     }
 
     public function testEmit()
