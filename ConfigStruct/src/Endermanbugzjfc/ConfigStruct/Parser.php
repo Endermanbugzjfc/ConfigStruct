3b06c2ae789b8e36153a10af411f347c893cfd99
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 22:40:48 +0800

    Added Parser.php

---
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/Parser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct;
+
+class Parser
+{
+
+
+    /**
+     * You shouldn't create an instance of this class, just run the APIs statically.
+     */
+    private function __construct()
+    {
+    }
+
+}
