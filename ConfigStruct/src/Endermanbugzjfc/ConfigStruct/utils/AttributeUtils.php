2070f42b6707fbeb9305523a35a0cc40f8bcf271
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 19:54:35 +0800

    Added documents for AttributeUtils.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/utils/AttributeUtils.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/utils/AttributeUtils.php
@@ -2,15 +2,27 @@
 
 namespace Endermanbugzjfc\ConfigStruct\utils;
 
+use ReflectionAttribute;
 use ReflectionProperty;
 
 final class AttributeUtils
 {
 
+    /**
+     * You shouldn't create an instance of this class, just run the APIs statically.
+     */
     private function __construct()
     {
     }
 
+    /**
+     * @param ReflectionProperty $property The property to check whether it has an instance of the given attribute.
+     * @param string $attribute The class string of an attribute.
+     * @phpstan-param class-string<Attribute>
+     * @param $value ReflectionAttribute A variable reference, the reflection instance of the given attribute will be assigned to this reference if found.
+     * @return bool True = the property doesn't have an instance of the given attribute.
+     * @noinspection PhpMissingParamTypeInspection stfu phpsjorm.
+     */
     public static function trueIfNoInProperty(
         ReflectionProperty $property,
         string             $attribute,
