9ee722b3993cc76a0655d7bb911712da10ef9ea4
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 19:59:19 +0800

    Added documents for Required.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Required.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Required.php
@@ -3,7 +3,13 @@
 namespace Endermanbugzjfc\ConfigStruct\attributes;
 
 use Attribute;
+use Endermanbugzjfc\ConfigStruct\exceptions\MissingFieldsException;
 
+/**
+ * When a property has this attribute, the corresponding field will be included in a {@link MissingFieldsException}. The exception will be thrown the parsing process has completed.
+ *
+ * This attribute has higher priority than {@link AutoInitializeChildStruct}.
+ */
 #[Attribute(Attribute::TARGET_PROPERTY)] class Required
 {
 
