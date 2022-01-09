afc82718ae30d3c4907e1a603f8a76d1e4bad6c6
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-09 20:30:14 +0800

    Added documents for KeyName.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/KeyName.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/KeyName.php
@@ -7,12 +7,20 @@ use Endermanbugzjfc\ConfigStruct\utils\AttributeUtils;
 use ReflectionAttribute;
 use ReflectionProperty;
 
+/**
+ * When a property has this attribute, its corresponding field will use the name given in the instance of this attribute instead of the property name.
+ *
+ * Unlimited key names (arguments) could be provided in the attribute. Each will be used for a deeper nesting level. Example: "a", "b" is actually [a => b].
+ *
+ * The nesting system can work with other properties too, the value won't be override unless the exact same key names are used in two different attributes, or two nodes have different data types.
+ * TODO: Examples
+ */
 #[Attribute(Attribute::TARGET_PROPERTY)] class KeyName
 {
 
     public function __construct(
         bool|int|float|string $name
-    )
+    ) // TODO: Update nesting system
     {
     }
 
