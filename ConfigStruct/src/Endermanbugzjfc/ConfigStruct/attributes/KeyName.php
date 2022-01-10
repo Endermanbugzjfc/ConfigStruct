4b75bbc6126b99f5913ba61db56137a64184bb36
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-10 19:24:21 +0800

    Removed nesting key name support

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/KeyName.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/KeyName.php
@@ -3,45 +3,34 @@
 namespace Endermanbugzjfc\ConfigStruct\attributes;
 
 use Attribute;
-use Endermanbugzjfc\ConfigStruct\utils\AttributeUtils;
-use ReflectionAttribute;
 use ReflectionProperty;
 
 /**
  * When a property has this attribute, its corresponding field will use the name given in the instance of this attribute instead of the property name.
- *
- * Unlimited key names (arguments) could be provided in the attribute. Each will be used for a deeper nesting level. Example: "a", "b" is actually [a => b].
- *
- * The nesting system can work with other properties too, the value won't be override unless the exact same key names are used in two different attributes, or two nodes have different data types.
- * TODO: Examples
  */
 #[Attribute(Attribute::TARGET_PROPERTY)] class KeyName
 {
 
     public function __construct(
         bool|int|float|string $name
-    ) // TODO: Update nesting system
+    )
     {
     }
 
+    /**
+     * @param ReflectionProperty $property
+     * @return bool|int|float|string|null
+     */
     public static function getFromProperty(
-        &$value,
         ReflectionProperty $property
-    ) : bool
+    ) : bool|int|float|string|null
     {
-        if (AttributeUtils::trueIfNoInProperty(
-            $property,
-            self::class,
-            $attribute
-        )) {
-            return false;
+        $attribute = $property->getAttributes(self::class)[0] ?? null;
+        if ($attribute === null) {
+            return $property->getName();
         }
-        assert($attribute instanceof ReflectionAttribute);
 
-
-        $value = $property->getName();
-        $value = $attribute->getArguments()[0];
-        return true;
+        return $attribute->getArguments()[0];
     }
 
 }
