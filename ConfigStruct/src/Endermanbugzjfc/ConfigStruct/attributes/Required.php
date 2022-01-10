dfa26337d498c0c2e041ecd1a2827e93752c28ad
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-10 19:31:43 +0800

    Attribute Required.php only functions when parsing

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Required.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Required.php
@@ -6,6 +6,8 @@ use Attribute;
 use Endermanbugzjfc\ConfigStruct\exceptions\MissingFieldsException;
 
 /**
+ * This attribute only functions when parsing.
+ *
  * When a property has this attribute, its corresponding field will be included in a {@link MissingFieldsException}. The exception will be thrown the parsing process has completed.
  */
 #[Attribute(Attribute::TARGET_PROPERTY)] class Required
