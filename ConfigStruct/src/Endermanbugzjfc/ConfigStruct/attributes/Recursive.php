3b4ad1aa9ad780df74e4350bb358c36bf3df0cb6
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-11 18:38:43 +0800

    Make attribute Recursive.php target class

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Recursive.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Recursive.php
@@ -6,11 +6,11 @@ use Attribute;
 use Endermanbugzjfc\ConfigStruct\exceptions\StructureException;
 
 /**
- * Recursive child struct check will not be performed on properties with this attribute.
+ * Recursive child struct check will not be performed on classes with this attribute.
  *
  * The initialization of child struct will also stop silently without throwing a {@link StructureException}.
  */
-#[Attribute(Attribute::TARGET_PROPERTY)] class Recursive
+#[Attribute(Attribute::TARGET_CLASS)] class Recursive
 {
 
     public function __construct()
