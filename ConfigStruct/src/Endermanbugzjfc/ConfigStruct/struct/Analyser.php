23f9b25fb99a146864dfb005fdc68efa4eceb59c
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-11 18:36:37 +0800

    Prepare to rewrite Analyser.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/struct/Analyser.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/struct/Analyser.php
@@ -7,9 +7,6 @@ use pocketmine\utils\Utils;
 use ReflectionClass;
 use ReflectionException;
 use ReflectionProperty;
-use ReflectionUnionType;
-use function class_exists;
-use function count;
 
 class Analyser
 {
@@ -39,57 +36,6 @@ class Analyser
                 ->getProperties(ReflectionProperty::IS_PUBLIC)
             as $property
         ) {
-            $type = $property->getType();
-            if ($type === null) {
-                continue;
-            } elseif ($type instanceof ReflectionUnionType) {
-                $types = $type->getTypes();
-            } else {
-                $types = [$type];
-            }
-            switch (count($types)) {
-                case 1:
-                    $sStruct = $types[0];
-                    break;
-                case 2:
-                    switch ("array") {
-                        case $types[0]:
-                            $sStruct = $types[1];
-                            break;
-                        case $types[1]:
-                            $sStruct = $types[0];
-                            break;
-                    }
-                    break;
-            }
-            if (
-                !isset($sStruct)
-                or
-                !class_exists($class = $sStruct->getName())
-            ) {
-                continue;
-            }
-            $init = true;
-            $sInstance = new $class;
-
-            $initialized = self::analyseStruct(
-                $sInstance,
-                $nodeTrace,
-                $initializeStruct
-            );
-
-            if ($initializeStruct) {
-                if (count($types) === 2) {
-                    $property->setValue(
-                        $struct,
-                        $initialized
-                            ? [$sInstance]
-                            : []
-                    );
-                } else {
-                    $property->setValue($struct, $sInstance);
-                }
-            }
         }
         return $init ?? false;
     }
