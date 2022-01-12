a77112f9a60751582f0c9bb52b07a5c7b40ba3be
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-13 02:06:18 +0800

    Updated MissingFieldsException.php to support the new codes

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/exceptions/MissingFieldsException.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/exceptions/MissingFieldsException.php
@@ -10,37 +10,68 @@ use function implode;
 class MissingFieldsException extends SetupException
 {
 
+    /**
+     * @param array<int|string> $fields Missing fields
+     * @param ReflectionProperty[] $properties The corresponding properties of the missing fields
+     */
     public function __construct(
-        protected array   $field,
-        protected ?string $configFile = null
+        protected array  $fields,
+        protected array  $properties,
+        protected string $configFile
     )
     {
-
-        $s = count($this->field) > 1;
-        $fields = implode(", ", array_map(function (string $field) {
-            return "\"$field\"";
-        }, $this->field));
-        parent::__construct(
-            "Required field$s $fields missing"
-            . (isset($this->configFile) ? " in $this->configFile" : ""),
-        );
+        $this->updateMessage();
     }
 
     /**
-     * @return ReflectionProperty
+     * @return array<int|string>
      */
-    public function getField() : ReflectionProperty
+    public function getFields() : array
     {
-        return $this->field;
+        return $this->fields;
     }
 
     /**
-     * @return string|null
+     * @return ReflectionProperty[]
      */
-    public function getConfigFile() : ?string
+    public function getProperties() : array
+    {
+        return $this->properties;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConfigFile() : string
     {
         return $this->configFile;
     }
 
+    /**
+     * @param string $configFile
+     */
+    public function setConfigFile(string $configFile) : void
+    {
+        $this->configFile = $configFile;
+        $this->updateMessage();
+    }
+
+    /**
+     * @return void Updates the exception message when its parameters changed.
+     */
+    protected function updateMessage() : void
+    {
+        $s = count($this->getFields()) > 1;
+        $fields = implode(", ", array_map(function (string $field) {
+            return "\"$field\"";
+        }, $this->getFields()));
+        parent::__construct(
+            "Required field$s $fields missing"
+            . ($this->getConfigFile() !== null
+                ? " in {$this->getConfigFile()}"
+                : ""
+            ),
+        );
+    }
 
 }
