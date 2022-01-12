d9e5a564d5521ea6c5c49d16aeaef12d6f077805
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-12 15:23:17 +0800

    Added AnalyserTest.php

---
+++ AnalyserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct\struct;
+
+use PHPUnit\Framework\TestCase;
+
+class AnalyserTest extends TestCase
+{
+
+    public function testAnalyseStruct()
+    {
+
+    }
+
+    public function testDoesKeyNameHaveDuplicatedArguments()
+    {
+
+    }
+
+    public function testWasKeyNameAlreadyUsed()
+    {
+
+    }
+
+    public function testDoesGroupPropertyHasInvalidType()
+    {
+
+    }
+}
