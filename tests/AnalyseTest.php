2b202c8d7f7709d14ea2ef8ed5c12f78e1a21c4c
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-13 18:21:19 +0800

    Implemented testDoesGroupPropertyHaveInvalidType()

--- tests/AnalyseTest.php
+++ tests/AnalyseTest.php
@@ -3,12 +3,43 @@
 namespace Endermanbugzjfc\ConfigStruct;
 
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use ReflectionProperty;
 
 class AnalyseTest extends TestCase
 {
 
     public function testDoesGroupPropertyHaveInvalidType()
     {
+        $a = new class() {
+            public $testNoType;
+            public mixed $testMixed;
+            public array $testArray;
+            public bool|int|float|string|array $testUnion;
+        };
+        foreach (
+            (new ReflectionClass($a))
+                ->getProperties(ReflectionProperty::IS_PUBLIC)
+            as $property
+        ) {
+            $this->assertNotTrue(
+                Analyse::doesGroupPropertyHaveInvalidType($property)
+            );
+        }
+
+        $b = new class() {
+            public string $testString;
+            public bool|int|float|string $testUnionIncompatible;
+        };
+        foreach (
+            (new ReflectionClass($b))
+                ->getProperties(ReflectionProperty::IS_PUBLIC)
+            as $property
+        ) {
+            $this->assertTrue(
+                Analyse::doesGroupPropertyHaveInvalidType($property)
+            );
+        }
 
     }
 
