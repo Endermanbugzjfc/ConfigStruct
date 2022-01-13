cd7837b1fc3347ef71a49e0d1386151d588efbd1
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-13 15:25:41 +0800

    Added AnalyseTest.php

---
+++ tests/AnalyseTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct;
+
+use PHPUnit\Framework\TestCase;
+
+class AnalyseTest extends TestCase
+{
+
+    public function testDoesGroupPropertyHaveInvalidType()
+    {
+
+    }
+
+    public function testRecursion()
+    {
+
+    }
+
+    public function testDoesKeyNameHaveDuplicatedArgument()
+    {
+
+    }
+
+    public function testStruct()
+    {
+
+    }
+
+    public function testProperty()
+    {
+
+    }
+}
