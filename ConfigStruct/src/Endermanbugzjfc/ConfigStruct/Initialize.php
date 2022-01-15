b7eddbe4be4dbb932f962b8f69fc8108720ab54b
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-16 04:59:55 +0800

    Added Initialize.php

---
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/Initialize.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct;
+
+final class Initialize
+{
+    /**
+     * @param object $struct The struct to be initialized, it should be already analysed using {@link Analyse::struct()}.
+     * @return void
+     */
+    public function struct(object $struct) : void
+    {
+    }
+
+}
