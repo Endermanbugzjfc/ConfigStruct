eb135a9cb85e91e38c0d8701485cdd13c93708f8
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-16 08:25:16 +0800

    Revert "Added a default values argument in Group.php"
    This reverts commit 481033d4a86a2153110c00db5c03ee55171cc44c.

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Group.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/Group.php
@@ -17,13 +17,10 @@ use Iterator;
      * @param string $class Class string of the child struct.
      * @phpstan-param class-string $class
      * @param int $wrapping How many [] should be wrapping the child struct, count from zero. Example: 0 = Struct[], 1 = Struct[][], etc...
-     * @param string ...$defaultValues Class name of structs that will be appended to the group as default value during initialize. Structure should be the same in the above struct and all default-value structs.
-     * @phpstan-param class-string $defaultValues
      */
     public function __construct(
         string $class,
-        int    $wrapping = 0,
-        string ...$defaultValues
+        int    $wrapping = 0
     )
     {
     }
