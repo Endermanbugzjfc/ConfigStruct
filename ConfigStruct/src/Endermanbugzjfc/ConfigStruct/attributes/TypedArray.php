6454c14eef4f415982868b82f9482ac0e4c6c98d
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-28 14:40:28 +0800

    Updated document and argument names in TypedArray.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/TypedArray.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/attributes/TypedArray.php
@@ -4,23 +4,24 @@ namespace Endermanbugzjfc\ConfigStruct\attributes;
 
 use ArrayAccess;
 use Attribute;
+use Endermanbugzjfc\ConfigStruct\Parse;
 use Iterator;
 
 #[Attribute(Attribute::TARGET_PROPERTY)] class TypedArray
 {
 
     /**
-     * A {@link TypedArray} refer to an array of child structs.
+     * This attribute is an indicator for {@link Parse} to convert array elements to child structs.
      *
      * Property that uses this attribute should has the type array or classes which implement the {@link ArrayAccess} and {@link Iterator} interface.
      *
      * @param string $class Class string of the child struct.
      * @phpstan-param class-string $class
-     * @param int $wrapping How many [] should be wrapping the child struct, count from zero. Example: 0 = Struct[], 1 = Struct[][], etc...
+     * @param int $dimension Example: 0 = Struct[], 1 = Struct[][], etc...
      */
     public function __construct(
         string $class,
-        int    $wrapping = 0
+        int $dimension = 0
     )
     {
     }
