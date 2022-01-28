5227035ace3af6e2c0e559496279bdf2da10a23b
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-28 11:48:21 +0800

    Maybe no, I want to make a safeAnalyse()

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/exceptions/StructureError.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/exceptions/StructureError.php
@@ -5,7 +5,7 @@ namespace Endermanbugzjfc\ConfigStruct\exceptions;
 use Endermanbugzjfc\ConfigStruct\Analyse;
 use Error;
 use Throwable;
-use const E_ERROR;
+use const E_RECOVERABLE_ERROR;
 
 /**
  * This error is thrown by {@link Analyse} when the structure of a struct class is invalid.
@@ -18,7 +18,7 @@ final class StructureError extends Error
     public function __construct(
         string     $message = "",
         ?Throwable $previous = null,
-        int        $code = E_ERROR
+        int $code = E_RECOVERABLE_ERROR
     )
     {
         parent::__construct($message, $code, $previous);
