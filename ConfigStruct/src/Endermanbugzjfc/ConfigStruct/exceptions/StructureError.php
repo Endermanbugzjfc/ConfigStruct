e478b439b27297032e84da2c1df3c55fc2ef0296
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-28 11:48:55 +0800

    The error will be used in other major API classes too

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/exceptions/StructureError.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/exceptions/StructureError.php
@@ -2,14 +2,11 @@
 
 namespace Endermanbugzjfc\ConfigStruct\exceptions;
 
-use Endermanbugzjfc\ConfigStruct\Analyse;
 use Error;
 use Throwable;
 use const E_RECOVERABLE_ERROR;
 
 /**
- * This error is thrown by {@link Analyse} when the structure of a struct class is invalid.
- *
  * This error shouldn't be caught. Direct change to the source code (structure of the struct class) should be made.
  */
 final class StructureError extends Error
