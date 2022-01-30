61c23f28d976b7a93b1558f470471197bfefd7bd
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-31 01:17:28 +0800

    Cleaned class for recode

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ChildStructParser.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ChildStructParser.php
@@ -2,41 +2,7 @@
 
 namespace Endermanbugzjfc\ConfigStruct\parse;
 
-use ReflectionNamedType;
-use function class_exists;
-
 class ChildStructParser
 {
 
-    public function isParserForProperty(
-        ParseProperty $property
-    ) : bool
-    {
-        $type = $property->getReflection()->getType();
-        if (!$type instanceof ReflectionNamedType) {
-            return false;
-        }
-
-        if (!class_exists($type->getName())) {
-            return false;
-        }
-
-        return true;
-    }
-
-    public function isParserForValue(
-        ParseProperty $property,
-        array         $value
-    ) : bool
-    {
-        return $this->isParserForProperty($property);
-    }
-
-    public function parseValue(
-        ParseProperty $property,
-        array         $value
-    ) : mixed
-    {
-        // TODO: Implement parseValue() method.
-    }
 }
