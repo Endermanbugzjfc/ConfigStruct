c06629cf4df79842affc2d6ab566a617b145d27e
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-31 01:59:18 +0800

    Same in StructParseOutput.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/StructParseOutput.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/StructParseOutput.php
@@ -2,40 +2,55 @@
 
 namespace Endermanbugzjfc\ConfigStruct\parse;
 
+use ReflectionClass;
 use ReflectionException;
 
 final class StructParseOutput
 {
 
     /**
+     * @param ReflectionClass $reflection
      * @param PropertyParseOutput[] $propertiesOutput
-     * @param PropertyParseOutput[] $unusedElements
+     * @param array $unusedElements
      * @param string[] $missingElements
      */
     private function __construct(
-        protected array $propertiesOutput,
-        protected array $unusedElements,
-        protected array $missingElements
+        protected ReflectionClass $reflection,
+        protected array           $propertiesOutput,
+        protected array           $unusedElements,
+        protected array           $missingElements
     )
     {
     }
 
     /**
+     * @param ReflectionClass $reflection
      * @param PropertyParseOutput[] $propertiesOutput
-     * @param PropertyParseOutput[] $unusedElements
+     * @param array $unusedElements
      * @param string[] $missingElements
+     * @return StructParseOutput
      */
     public static function create(
-        array $propertiesOutput,
-        array $unusedElements,
-        array $missingElements
+        ReflectionClass $reflection,
+        array           $propertiesOutput,
+        array           $unusedElements,
+        array           $missingElements
     ) : self
     {
         return new self(
+            $reflection,
             $propertiesOutput,
             $unusedElements,
             $missingElements
         );
+    }
+
+    /**
+     * @return ReflectionClass
+     */
+    public function getReflection() : ReflectionClass
+    {
+        return $this->reflection;
     }
 
     /**
