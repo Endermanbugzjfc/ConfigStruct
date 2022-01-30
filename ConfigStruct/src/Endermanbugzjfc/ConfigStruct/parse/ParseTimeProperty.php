302d2c4e0b9be560f61f678df96b71842da0f4ae
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-30 14:21:19 +0800

    Added basic methods for ParseTimeProperty.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseTimeProperty.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseTimeProperty.php
@@ -2,7 +2,31 @@
 
 namespace Endermanbugzjfc\ConfigStruct\parse;
 
+use ReflectionProperty;
+
 final class ParseTimeProperty
 {
+
+    private function __construct(
+        protected ReflectionProperty $reflection
+    )
+    {
+
+    }
+
+    public static function fromReflection(
+        ReflectionProperty $reflection
+    ) : self
+    {
+        return new self($reflection);
+    }
+
+    /**
+     * @return ReflectionProperty
+     */
+    public function getReflection() : ReflectionProperty
+    {
+        return $this->reflection;
+    }
 
 }
