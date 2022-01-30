31c3b95ca5b291f7453cfe2f02c68453a91e086d
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-30 20:11:09 +0800

    Added more stuff in ParseResultForProperty.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseResultForProperty.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseResultForProperty.php
@@ -2,16 +2,65 @@
 
 namespace Endermanbugzjfc\ConfigStruct\parse;
 
+use Endermanbugzjfc\ConfigStruct\parse\special\SpecialParserInterface;
+
 final class ParseResultForProperty
 {
 
-    private function __construct()
+    /**
+     * @param array $rawOutput
+     * @param ParseResultForStruct[] $childStructs
+     * @param SpecialParserInterface|null $specialParser
+     */
+    private function __construct(
+        protected array                   $rawOutput,
+        protected array                   $childStructs,
+        protected ?SpecialParserInterface $specialParser
+    )
     {
     }
 
-    public static function create() : self
+    /**
+     * @param array $rawOutput
+     * @param ParseResultForStruct[] $childStructs
+     * @param SpecialParserInterface|null $specialParser
+     * @return ParseResultForProperty
+     */
+    public static function create(
+        array                   $rawOutput,
+        array                   $childStructs,
+        ?SpecialParserInterface $specialParser
+    ) : self
     {
-        return new self();
+        return new self(
+            $rawOutput,
+            $childStructs,
+            $specialParser
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getRawOutput() : array
+    {
+        return $this->rawOutput;
+    }
+
+    /**
+     * @return ParseResultForStruct[]
+     */
+    public function getChildStructs() : array
+    {
+        return $this->childStructs;
+    }
+
+    /**
+     * @return SpecialParserInterface|null
+     */
+    public function getSpecialParser() : ?SpecialParserInterface
+    {
+        return $this->specialParser;
     }
 
 }
