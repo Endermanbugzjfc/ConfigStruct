494b91be2ea57f9be239a801423a63b3aeed335b
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-30 20:41:11 +0800

    Bind property to ParseOutputProperty.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseOutputProperty.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseOutputProperty.php
@@ -8,11 +8,13 @@ final class ParseOutputProperty
 {
 
     /**
+     * @param ParseTimeProperty $property
      * @param array $finalizedValues
      * @param ParseOutputStruct[] $childStructs
      * @param SpecialParserInterface|null $specialParser
      */
     private function __construct(
+        protected ParseTimeProperty       $property,
         protected array                   $finalizedValues,
         protected array                   $childStructs,
         protected ?SpecialParserInterface $specialParser
@@ -21,18 +23,21 @@ final class ParseOutputProperty
     }
 
     /**
+     * @param ParseTimeProperty $property
      * @param array $finalizedValues
      * @param ParseOutputStruct[] $childStructs
      * @param SpecialParserInterface|null $specialParser
      * @return ParseOutputProperty
      */
     public static function create(
+        ParseTimeProperty       $property,
         array                   $finalizedValues,
         array                   $childStructs,
         ?SpecialParserInterface $specialParser
     ) : self
     {
         return new self(
+            $property,
             $finalizedValues,
             $childStructs,
             $specialParser
@@ -53,6 +58,14 @@ final class ParseOutputProperty
     public function getSpecialParser() : ?SpecialParserInterface
     {
         return $this->specialParser;
+    }
+
+    /**
+     * @return ParseTimeProperty
+     */
+    public function getProperty() : ParseTimeProperty
+    {
+        return $this->property;
     }
 
 }
