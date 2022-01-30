3461640cea882ec16d674a0636beacaa204df119
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-30 14:48:31 +0800

    Implemented bindKeyNameIgnoreExistenceInData()

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseTimeProperty.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseTimeProperty.php
@@ -4,9 +4,12 @@ namespace Endermanbugzjfc\ConfigStruct\parse;
 
 use Endermanbugzjfc\ConfigStruct\KeyName;
 use ReflectionProperty;
+use function array_key_exists;
 
 final class ParseTimeProperty
 {
+
+    protected mixed $boundKeyName;
 
     private function __construct(
         protected ParseTimeStruct    $owner,
@@ -38,6 +41,34 @@ final class ParseTimeProperty
     public function getOwner() : ParseTimeStruct
     {
         return $this->owner;
+    }
+
+    public function bindKeyNameIgnoreExistenceInData(
+        array $data
+    ) : string
+    {
+        foreach ($this->getKeyNameCandidates() as $name) {
+            if (array_key_exists($name, $data)) {
+                return $this->boundKeyName = $name;
+            }
+        }
+        return $this->getReflection()->getName();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBoundKeyName() : mixed
+    {
+        return $this->boundKeyName;
+    }
+
+    /**
+     * @param mixed $boundKeyName
+     */
+    public function setBoundKeyName(mixed $boundKeyName) : void
+    {
+        $this->boundKeyName = $boundKeyName;
     }
 
     public function getKeyNameCandidates() : array
