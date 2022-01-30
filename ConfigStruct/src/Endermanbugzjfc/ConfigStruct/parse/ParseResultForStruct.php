772f314ae4ae5274947330953b8d96e96c7072f7
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-30 16:05:46 +0800

    Added unused and missing elements

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseResultForStruct.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseResultForStruct.php
@@ -5,13 +5,43 @@ namespace Endermanbugzjfc\ConfigStruct\parse;
 final class ParseResultForStruct
 {
 
-    private function __construct()
+    private function __construct(
+        protected array $unusedElements,
+        protected array $missingElements
+    )
     {
     }
 
-    public static function create() : self
+    /**
+     * @param array $unusedElements
+     * @param ParseTimeProperty[] $missingElements
+     * @return static
+     */
+    public static function create(
+        array $unusedElements,
+        array $missingElements
+    ) : self
     {
-        return new self();
+        return new self(
+            $unusedElements,
+            $missingElements
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getUnusedElements() : array
+    {
+        return $this->unusedElements;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMissingElements() : array
+    {
+        return $this->missingElements;
     }
 
 }
