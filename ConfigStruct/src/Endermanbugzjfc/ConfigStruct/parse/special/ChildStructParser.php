86c053d68475f0f604773a268a8ea28b6829ae9e
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-30 15:35:07 +0800

    Implemented value filters for ChildStructParser.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/special/ChildStructParser.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/special/ChildStructParser.php
@@ -3,13 +3,24 @@
 namespace Endermanbugzjfc\ConfigStruct\parse\special;
 
 use Endermanbugzjfc\ConfigStruct\parse\ParseTimeProperty;
+use ReflectionNamedType;
+use function class_exists;
 
 class ChildStructParser implements SpecialParserInterface
 {
 
     public function isParserForProperty(ParseTimeProperty $property) : bool
     {
-        // TODO: Implement isParserForProperty() method.
+        $type = $property->getReflection()->getType();
+        if (!$type instanceof ReflectionNamedType) {
+            return false;
+        }
+
+        if (!class_exists($type->getName())) {
+            return false;
+        }
+
+        return true;
     }
 
     public function isParserForValue(
@@ -17,7 +28,7 @@ class ChildStructParser implements SpecialParserInterface
         array             $value
     ) : bool
     {
-        // TODO: Implement isParserForValue() method.
+        return $this->isParserForProperty($property);
     }
 
     public function parseValue(array $value) : mixed
