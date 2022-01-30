9648656f7bcf7856b2a16e05e134fe9266ef6091
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-30 14:43:03 +0800

    Implemented getKeyNameCandidates()

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseTimeProperty.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseTimeProperty.php
@@ -2,6 +2,7 @@
 
 namespace Endermanbugzjfc\ConfigStruct\parse;
 
+use Endermanbugzjfc\ConfigStruct\KeyName;
 use ReflectionProperty;
 
 final class ParseTimeProperty
@@ -37,6 +38,16 @@ final class ParseTimeProperty
     public function getOwner() : ParseTimeStruct
     {
         return $this->owner;
+    }
+
+    public function getKeyNameCandidates() : array
+    {
+        foreach ($this->getReflection()->getAttributes(
+            KeyName::class
+        ) as $keyName) {
+            $return = $keyName->getArguments()[0];
+        }
+        return $return ?? [];
     }
 
 }
