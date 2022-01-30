b4f955cf4cbcb863ae56e16bcb0bbd2e7e6fe26c
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-31 02:43:26 +0800

    Added prototype of parseStruct()

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/StructParser.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/StructParser.php
@@ -2,6 +2,13 @@
 
 namespace Endermanbugzjfc\ConfigStruct\parse;
 
+use ReflectionClass;
+use ReflectionProperty;
+use function array_diff;
+use function array_key_exists;
+use function array_keys;
+use function array_values;
+
 final class StructParser
 {
 
@@ -10,6 +17,58 @@ final class StructParser
      */
     private function __construct()
     {
+    }
+
+    public static function parseStruct(
+        ReflectionClass $reflection,
+        array           $input
+    ) : StructParseOutput
+    {
+        $properties = $reflection->getProperties(
+            ReflectionProperty::IS_PUBLIC
+        );
+        $map = self::getPropertyNameToKeyNameMap(
+            $properties,
+            $input
+        );
+
+        foreach ($properties as $property) {
+            $key = $map[$property->getName()]:
+            if (!array_key_exists(
+                $key,
+                $input
+            )) {
+                $missing[$property->getName()] = $property;
+                continue;
+            }
+            self::parseProperty($property, $key);
+        }
+
+        return StructParseOutput::create(
+            $reflection,
+            [],
+            array_diff(
+                array_keys($input),
+                array_values($map)
+            ),
+            $missing ?? []
+        );
+    }
+
+    public static function getPropertyNameToKeyNameMap(
+        array $properties,
+        array $input
+    ) : array
+    {
+
+    }
+
+    public static function parseProperty(
+        ReflectionProperty $property,
+        mixed              $key
+    ) : PropertyParseOutput
+    {
+
     }
 
 }
