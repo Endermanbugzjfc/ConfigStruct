f0c8c0436d01521b3da8538326d507389465635f
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-30 14:23:37 +0800

    Added basic methods for ParseTimeStruct.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseTimeStruct.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseTimeStruct.php
@@ -2,7 +2,31 @@
 
 namespace Endermanbugzjfc\ConfigStruct\parse;
 
+use ReflectionClass;
+
 final class ParseTimeStruct
 {
+
+    private function __construct(
+        protected ReflectionClass $reflection
+    )
+    {
+
+    }
+
+    public static function fromReflection(
+        ReflectionClass $reflection
+    ) : self
+    {
+        return new self($reflection);
+    }
+
+    /**
+     * @returnReflectionClass
+     */
+    public function getReflection() : ReflectionClass
+    {
+        return $this->reflection;
+    }
 
 }
