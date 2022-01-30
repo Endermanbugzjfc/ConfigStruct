ac0dec1ffa049ad3e83d4fbbd76584113d837a39
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-30 20:33:06 +0800

    ParseOutputStruct.php should also hold the owner struct

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseOutputStruct.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseOutputStruct.php
@@ -6,31 +6,36 @@ final class ParseOutputStruct
 {
 
     /**
+     * @param ParseTimeStruct $struct
      * @param ParseOutputProperty[] $properties
      * @param array $unusedElements
      * @param ParseTimeProperty[] $missingElements
      */
     private function __construct(
-        protected array $properties,
-        protected array $unusedElements,
-        protected array $missingElements
+        protected ParseTimeStruct $struct,
+        protected array           $properties,
+        protected array           $unusedElements,
+        protected array           $missingElements
     )
     {
     }
 
     /**
+     * @param ParseTimeStruct $struct
      * @param ParseOutputProperty[] $properties
      * @param array $unusedElements
      * @param ParseTimeProperty[] $missingElements
      * @return static
      */
     public static function create(
-        array $properties,
-        array $unusedElements,
-        array $missingElements
+        ParseTimeStruct $struct,
+        array           $properties,
+        array           $unusedElements,
+        array           $missingElements
     ) : self
     {
         return new self(
+            $struct,
             $properties,
             $unusedElements,
             $missingElements
@@ -59,6 +64,14 @@ final class ParseOutputStruct
     public function getProperties() : array
     {
         return $this->properties;
+    }
+
+    /**
+     * @return ParseTimeStruct
+     */
+    public function getStruct() : ParseTimeStruct
+    {
+        return $this->struct;
     }
 
 }
