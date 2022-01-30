6403528ddd7cfee5cd79f92a73ae96b7398091ae
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-31 01:29:31 +0800

    Implemented copyToObject() in ParseOutput.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseOutput.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseOutput.php
@@ -2,9 +2,20 @@
 
 namespace Endermanbugzjfc\ConfigStruct\parse;
 
+use ReflectionProperty;
+
 abstract class ParseOutput
 {
 
     abstract protected function getFlattenedValue() : mixed;
+
+    public function copyToObject(
+        object             $object,
+        ReflectionProperty $property
+    ) : object
+    {
+        $property->setValue($object, $this->getFlattenedValue());
+        return $object;
+    }
 
 }
