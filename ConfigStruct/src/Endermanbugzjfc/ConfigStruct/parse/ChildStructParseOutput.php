294413b795fa73a1a4f73508453b80858c7954c2
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-31 01:21:17 +0800

    Added initializer methods

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ChildStructParseOutput.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ChildStructParseOutput.php
@@ -5,4 +5,19 @@ namespace Endermanbugzjfc\ConfigStruct\parse;
 final class ChildStructParseOutput implements ValueOutputInterface
 {
 
+    private function __construct(
+        protected ParseOutputStruct $childStruct
+    )
+    {
+    }
+
+    public static function create(
+        ParseOutputStruct $childStruct
+    ) : self
+    {
+        return new self(
+            $childStruct
+        );
+    }
+
 }
