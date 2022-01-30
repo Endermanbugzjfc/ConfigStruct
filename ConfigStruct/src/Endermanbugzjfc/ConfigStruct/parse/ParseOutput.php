ea698a9458ef5fc8dd212fddd12598be745699ab
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-01-31 02:05:29 +0800

    Merged PropertyParseOutput.php into ParseOutput.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseOutput.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParseOutput.php
@@ -10,12 +10,40 @@ abstract class ParseOutput
     abstract protected function getFlattenedValue() : mixed;
 
     public function copyToObject(
-        object             $object,
-        ReflectionProperty $property
+        object $object,
     ) : object
     {
-        $property->setValue($object, $this->getFlattenedValue());
+        $this->getReflection()->setValue($object, $this->getFlattenedValue());
         return $object;
+    }
+
+    /**
+     * @param ReflectionProperty $reflection
+     * @param string $keyName
+     * @param mixed $output
+     */
+    private function __construct(
+        protected ReflectionProperty $reflection,
+        protected string             $keyName,
+        protected mixed              $output
+    )
+    {
+    }
+
+    /**
+     * @return ReflectionProperty
+     */
+    public function getReflection() : ReflectionProperty
+    {
+        return $this->reflection;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKeyName() : string
+    {
+        return $this->keyName;
     }
 
 }
