2ab808c31b2f7cef6ef74036c02511a596dc689a
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 00:48:13 +0800

    Implemented StructEmitOutput.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/StructEmitOutput.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/StructEmitOutput.php
@@ -2,7 +2,65 @@
 
 namespace Endermanbugzjfc\ConfigStruct\emit;
 
+use ReflectionClass;
+
 final class StructEmitOutput
 {
+
+    /**
+     * @param ReflectionClass $reflection
+     * @param PropertyEmitOutput[] $propertiesOutput
+     * @param string[] $skippedEmptyProperties
+     */
+    private function __construct(
+        protected ReflectionClass $reflection,
+        protected array           $propertiesOutput,
+        protected array           $skippedEmptyProperties
+    )
+    {
+    }
+
+    /**
+     * @param ReflectionClass $reflection
+     * @param PropertyEmitOutput[] $propertiesOutput
+     * @param string[] $skippedEmptyProperties
+     * @return StructEmitOutput
+     */
+    public static function create(
+        ReflectionClass $reflection,
+        array           $propertiesOutput,
+        array           $skippedEmptyProperties
+    ) : self
+    {
+        return new self(
+            $reflection,
+            $propertiesOutput,
+            $skippedEmptyProperties
+        );
+    }
+
+    /**
+     * @return ReflectionClass
+     */
+    public function getReflection() : ReflectionClass
+    {
+        return $this->reflection;
+    }
+
+    /**
+     * @return PropertyEmitOutput[]
+     */
+    public function getPropertiesOutput() : array
+    {
+        return $this->propertiesOutput;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getSkippedEmptyProperties() : array
+    {
+        return $this->skippedEmptyProperties;
+    }
 
 }
