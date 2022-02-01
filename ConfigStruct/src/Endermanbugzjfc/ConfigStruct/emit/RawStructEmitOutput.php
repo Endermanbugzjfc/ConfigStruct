dc26f872805bee20cbef982a6d72ade0a8ff6034
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 00:21:46 +0800

    Did a accidentally staged this file in one of the previous commits

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/RawStructEmitOutput.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/RawStructEmitOutput.php
@@ -11,15 +11,13 @@ final class RawStructEmitOutput extends PropertyParseOutput
     public static function create(
         ReflectionProperty $reflection,
         string             $keyName,
-        mixed              $output,
-        array              $exceptions
+        mixed $output
     ) : PropertyParseOutput
     {
         return new self(
             $reflection,
             $keyName,
-            $output,
-            $exceptions
+            $output
         );
     }
 
