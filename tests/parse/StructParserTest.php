eb1fe230e20d576216f6ba03858ded2ae682ae58
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-01 23:13:38 +0800

    Added StructParserTest.php

---
+++ tests/parse/StructParserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct\parse;
+
+use PHPUnit\Framework\TestCase;
+
+class StructParserTest extends TestCase
+{
+
+    public function testParseProperty()
+    {
+
+    }
+
+    public function testParseStruct()
+    {
+
+    }
+
+    public function testGetPropertyNameToKeyNameMap()
+    {
+
+    }
+
+    public function testParseChildStruct()
+    {
+
+    }
+}
