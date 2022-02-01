338d1c355c8a026ed9166c987659749d0f3fef11
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 00:11:21 +0800

    Implemented RawStructEmitOutput.php

---
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/RawStructEmitOutput.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct\emit;
+
+use Endermanbugzjfc\ConfigStruct\parse\PropertyParseOutput;
+use ReflectionProperty;
+
+class RawStructEmitOutput extends PropertyParseOutput
+{
+
+    public static function create(
+        ReflectionProperty $reflection,
+        string             $keyName,
+        mixed              $output,
+        array              $exceptions
+    ) : PropertyParseOutput
+    {
+        return new self(
+            $reflection,
+            $keyName,
+            $output,
+            $exceptions
+        );
+    }
+
+    protected function getFlattenedValue() : mixed
+    {
+        return $this->output;
+    }
+}
