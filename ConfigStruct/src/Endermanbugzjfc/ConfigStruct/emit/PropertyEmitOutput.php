239ff4e351ac9500fb5b68157f5163a12284506d
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-01 23:59:26 +0800

    Implemented PropertyEmitOutput.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/PropertyEmitOutput.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/PropertyEmitOutput.php
@@ -2,7 +2,43 @@
 
 namespace Endermanbugzjfc\ConfigStruct\emit;
 
+use Endermanbugzjfc\ConfigStruct\KeyName;
+use ReflectionProperty;
+
 abstract class PropertyEmitOutput
 {
+
+    abstract public function getFlattenedValues() : mixed;
+
+    protected function __construct(
+        protected ReflectionProperty $reflection,
+        protected mixed              $output
+    )
+    {
+    }
+
+    abstract public static function create(
+        ReflectionProperty $reflection,
+        mixed              $output
+    ) : self;
+
+    /**
+     * @return ReflectionProperty
+     */
+    public function getReflection() : ReflectionProperty
+    {
+        return $this->reflection;
+    }
+
+    public function getKeyName() : string
+    {
+        $keyName = $this->getReflection()->getAttributes(
+                KeyName::class
+            )[0] ?? null;
+        if ($keyName !== null) {
+            return $keyName;
+        }
+        return $this->getReflection()->getName();
+    }
 
 }
