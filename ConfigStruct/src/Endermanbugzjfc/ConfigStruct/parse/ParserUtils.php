637750dadecaa16e089179ee19bfd92ebc03af60
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 17:43:46 +0800

    Added ParserUtils.php

---
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/parse/ParserUtils.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct\parse;
+
+final class ParserUtils
+{
+
+    /**
+     * This class should be used statically!
+     */
+    private function __construct()
+    {
+    }
+
+}
