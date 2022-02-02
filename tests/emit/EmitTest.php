a60f4905cae0c7514c3d25c1d770c34bdbea76b5
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 21:59:47 +0800

    Implemented testEmitStructPropertySkipping()

--- tests/emit/EmitTest.php
+++ tests/emit/EmitTest.php
@@ -7,9 +7,39 @@ use PHPUnit\Framework\TestCase;
 class EmitTest extends TestCase
 {
 
-    public function testEmitStructAccessLevel()
+    public function testEmitStructPropertySkipping()
     {
+        $object = new class() {
 
+            private bool $testPrivateProperty = true;
+            protected bool $testProtectedProperty = true;
+
+            public bool $testNotInitialized;
+
+            public $testNoType;
+            public bool $testDefaultValue = true;
+            public ?bool $testNull = null;
+
+        };
+        $output = Emit::emitStruct($object);
+        $this->assertTrue(
+            $output->getFlattenedValue() === [
+                "testNoType" => null,
+                "testDefaultValue" => true,
+                "testNull" => null
+            ]
+        );
+
+        $skipped = $output->getSkippedEmptyProperties();
+        $this->assertTrue(
+            count($skipped) === 1
+        );
+        $this->assertTrue(
+            $skipped
+            ["testNotInitialized"]
+                ->getName()
+            === "testNotInitialized"
+        );
     }
 
 }
