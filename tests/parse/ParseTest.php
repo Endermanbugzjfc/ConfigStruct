094207fec42336b7821163ae4f6a005431ac4c97
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 18:11:38 +0800

    Added ParseTest.php

---
+++ tests/parse/ParseTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct\parse;
+
+use PHPUnit\Framework\TestCase;
+
+class ParseTest extends TestCase
+{
+
+    public function testParseStruct()
+    {
+
+    }
+}
