305a3257ab7963aade298ac67027b27ca3bcf690
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 18:21:07 +0800

    Implemented testParseStructPropertyAccessLevels()

--- tests/parse/ParseTest.php
+++ tests/parse/ParseTest.php
@@ -7,8 +7,39 @@ use PHPUnit\Framework\TestCase;
 class ParseTest extends TestCase
 {
 
-    public function testParseStruct()
+    public function testParseStructPropertyAccessLevels()
     {
+        $object = new class() {
 
+            private $testPrivateProperty;
+
+            protected $testProtectedProperty;
+
+            public $testPublicProperty;
+
+        };
+        $clone = clone $object;
+        $output = Parse::parseStruct(
+            $object,
+            [
+                "testPrivateProperty" => null,
+                "testProtectedProperty" => null,
+                "testPublicProperty" => null
+            ]
+        );
+
+        $this->assertTrue(
+            count($output->getPropertiesOutput()) === 1
+        );
+
+        $property = $output->getPropertiesOutput()[0];
+        $this->assertTrue(
+            $property->getKeyName() === "testPublicProperty"
+        );
+
+        $clone->testPublicProperty = null;
+        $this->assertTrue(
+            $object == $clone
+        );
     }
 }
