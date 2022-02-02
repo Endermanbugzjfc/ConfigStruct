bdf46b449041517fe440e6183cd703658ae5a430
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 21:49:56 +0800

    Added EmitTest.php

---
+++ tests/emit/EmitTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct\emit;
+
+use PHPUnit\Framework\TestCase;
+
+class EmitTest extends TestCase
+{
+
+    public function testEmitStruct()
+    {
+
+    }
+
+    public function testEmitProperty()
+    {
+
+    }
+
+    public function testEmitChildStruct()
+    {
+
+    }
+}
