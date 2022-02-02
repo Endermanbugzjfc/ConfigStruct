d336d36dfba1dcbe4ca4b4c7b2a8dbe59c5d9787
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 22:19:53 +0800

    Ah yes php sjom
    - A retarded IDE designed for a retarded language

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/StructureException.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/StructureException.php
@@ -2,14 +2,14 @@
 
 namespace Endermanbugzjfc\ConfigStruct;
 
-use PHPUnit\Framework\Exception;
+use RuntimeException;
 use Throwable;
 use const E_ERROR;
 
 /**
  * This exception should neither be recovered nor be caught. Direct change to the source code (structure of the struct class) should be made.
  */
-final class StructureException extends Exception
+final class StructureException extends RuntimeException
 {
 
     public function __construct(
