d3110fd18e6ddd1d59ae88328f317e9402f9360f
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 23:26:27 +0800

    Added document for PropertyEmitOutput.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/PropertyEmitOutput.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/PropertyEmitOutput.php
@@ -3,6 +3,7 @@
 namespace Endermanbugzjfc\ConfigStruct\emit;
 
 use Endermanbugzjfc\ConfigStruct\KeyName;
+use Endermanbugzjfc\ConfigStruct\parse\PropertyParseOutput;
 use ReflectionProperty;
 
 abstract class PropertyEmitOutput
@@ -10,6 +11,10 @@ abstract class PropertyEmitOutput
 
     abstract public function getFlattenedValue() : mixed;
 
+    /**
+     * @param ReflectionProperty $reflection Reflection of the property.
+     * @param mixed $Parse Emit output which haven't been flattened.
+     */
     protected function __construct(
         protected ReflectionProperty $reflection,
         protected mixed              $output
@@ -23,13 +28,16 @@ abstract class PropertyEmitOutput
     ) : self;
 
     /**
-     * @return ReflectionProperty
+     * @return ReflectionProperty Reflection of the property.
      */
     final public function getReflection() : ReflectionProperty
     {
         return $this->reflection;
     }
 
+    /**
+     * @return string The property name or the first custom key name candidate if there is one. Unlike {@link PropertyParseOutput::getKeyName()}, the return value of this function should always be consistent for the same property.
+     */
     public function getKeyName() : string
     {
         $keyName = $this->getReflection()->getAttributes(
