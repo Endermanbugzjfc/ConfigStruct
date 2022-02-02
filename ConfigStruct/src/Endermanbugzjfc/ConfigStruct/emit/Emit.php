425497df4171762818e55fc207bd6b6a1c1cbce3
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-02 20:20:45 +0800

    Cleaned up Emit.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/Emit.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/Emit.php
@@ -2,9 +2,6 @@
 
 namespace Endermanbugzjfc\ConfigStruct\emit;
 
-use ReflectionClass;
-use ReflectionProperty;
-
 final class Emit
 {
 
@@ -13,23 +10,6 @@ final class Emit
      */
     private function __construct()
     {
-    }
-
-    /**
-     * Look at README.md for examples.
-     *
-     * @param object $struct An instance of your config struct class. Values of its initiated properties, or uninitiated struct class properties with an {@link AutoInitializeChildStruct} attribute will be encoded recursively in the given type (language) in the form of nested scalar keys-values array and be returned.
-     * @return array Return a nested scalar keys-values array which holds the encoded content.
-     */
-    public static function array(object $struct) : array
-    {
-        foreach (
-            (new ReflectionClass($struct))
-                ->getProperties(ReflectionProperty::IS_PUBLIC)
-            as $property
-        ) {
-        }
-        return $array ?? [];
     }
 
 }
