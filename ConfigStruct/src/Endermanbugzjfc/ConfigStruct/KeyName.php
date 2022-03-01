95ebc29f0a2bc3278ca6b6556426bc194a7dd42a
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-02 00:58:37 +0800

    bro this doc confusing

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/KeyName.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/KeyName.php
@@ -7,7 +7,7 @@ use Attribute;
 /**
  * Emit and parse this property with the name provided in the attribute instead of the property name.
  *
- * If multiple names are provided, only the first one is used for emitting, and the first name available in the input is used for parsing. This gets useful when updating configs.
+ * If multiple names are provided, only the first one is used for emitting, and the first name available in the input (the first key that exists in the parse data) is used for parsing. This gets useful when updating configs.
  */
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 class KeyName
