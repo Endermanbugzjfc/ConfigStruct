5faf8fbde37a45b2d398af312d11637c36d9214c
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-02 00:20:32 +0800

    Added EmitUtils.php

---
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/EmitUtils.php
@@ -0,0 +1,18 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ConfigStruct\emit;
+
+final class EmitUtils
+{
+
+    /**
+     * This class should be used statically!
+     */
+    private function __construct()
+    {
+    }
+
+}
