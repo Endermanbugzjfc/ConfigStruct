d95bfad4810ada67a50dc377d9fe1a28e2d80bc7
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-02 00:27:27 +0800

    Implemented filterNonPublic()

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/EmitUtils.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/emit/EmitUtils.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ConfigStruct\emit;
 
+use function str_contains;
+
 final class EmitUtils
 {
 
@@ -13,6 +15,26 @@ final class EmitUtils
      */
     private function __construct()
     {
+    }
+
+    /**
+     * Filter non-public properties from an array that is converted from object.
+     * @param array $array
+     * @return void
+     */
+    public static function filterNonPublic(
+        array &$array
+    ) : void {
+        foreach ($array as $key => $value) {
+            if (str_contains(
+                $key,
+                "\000"
+            )) {
+                unset(
+                    $array[$key]
+                );
+            }
+        }
     }
 
 }
