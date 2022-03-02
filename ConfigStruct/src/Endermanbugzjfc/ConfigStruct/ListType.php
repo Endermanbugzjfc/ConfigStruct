261195ec48f9c2be2d67c0ce0c3e40f7b62b8658
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-02 19:36:54 +0800

    Documented ListType.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ListType.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ListType.php
@@ -5,7 +5,9 @@ namespace Endermanbugzjfc\ConfigStruct;
 use Attribute;
 
 /**
- * TODO
+ * Struct candidate for elements in a list.
+ *
+ * If multiple candidates are provided, only the one with least unhandled elements will be used. Incompatible structs will never be used. If there is no suitable struct for an element, that element will not be parsed, raw values will be returned.
  */
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 class ListType
