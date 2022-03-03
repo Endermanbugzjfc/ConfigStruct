6995d3d0abd66d32429920788c90a60becb6ffcb
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-03 21:52:02 +0800

    Implemented getErrors() in RawContext.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/RawContext.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/RawContext.php
@@ -2,20 +2,30 @@
 
 namespace Endermanbugzjfc\ConfigStruct\ParseContext;
 
+use Throwable;
+
 final class RawContext extends BasePropertyContext
 {
     use NonAbstractContextTrait;
 
     protected mixed $value;
 
+    /**
+     * @param BasePropertyContext $context
+     * @param mixed $value
+     * @param Throwable[] $errors
+     * @return static
+     */
     public static function create(
         BasePropertyContext $context,
-        mixed $value
+        mixed               $value,
+        array               $errors
     ) : self
     {
         $self = new self();
         $self->substitute($context);
         $self->value = $value;
+        $self->errors = $errors;
 
         return $self;
     }
@@ -27,6 +37,14 @@ final class RawContext extends BasePropertyContext
     public function getValue() : mixed
     {
         return $this->value;
+    }
+
+    /**
+     * @return Throwable[]
+     */
+    public function getErrors() : array
+    {
+        return $this->errors;
     }
 
 }
