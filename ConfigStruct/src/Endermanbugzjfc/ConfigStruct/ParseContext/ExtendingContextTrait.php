d6dd812c1cba9f27821ea9e90af01e787e4de066
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-03 17:54:12 +0800

    Force classes that use this trait to implement getValue() and create()

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/ExtendingContextTrait.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/ExtendingContextTrait.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ConfigStruct\ParseContext;
 
+use RuntimeException;
+
 trait ExtendingContextTrait
 {
 
@@ -12,6 +14,18 @@ trait ExtendingContextTrait
      */
     private function __construct()
     {
+    }
+
+    public function getValue() : mixed {
+        throw new RuntimeException(
+            "Implement me"
+        );
+    }
+
+    public static function create() : self {
+        throw new RuntimeException(
+            "Implement me"
+        );
     }
 
 }
