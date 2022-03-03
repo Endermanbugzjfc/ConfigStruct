cdefd9809973ec4d7e31bb8d4bb31a4f3be99e83
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-03 18:40:53 +0800

    Override getErrors() in ChildObjectContext.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/ChildObjectContext.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/ChildObjectContext.php
@@ -2,6 +2,9 @@
 
 namespace Endermanbugzjfc\ConfigStruct\ParseContext;
 
+use Throwable;
+use function array_merge;
+
 final class ChildObjectContext extends BasePropertyContext
 {
     use NonAbstractContextTrait;
@@ -39,6 +42,24 @@ final class ChildObjectContext extends BasePropertyContext
     public function asObjectContext() : ObjectContext
     {
         return $this->objectContext;
+    }
+
+    /**
+     * @return Throwable[]
+     */
+    public function getErrors() : array
+    {
+        $errs = [];
+        $properties = $this->asObjectContext()->getErrorProperties();
+        foreach ($properties as $property) {
+            $propertyErrs = $property->getErrors();
+            $errs = array_merge(
+                $errs,
+                $propertyErrs
+            );
+        }
+
+        return $errs;
     }
 
 }
