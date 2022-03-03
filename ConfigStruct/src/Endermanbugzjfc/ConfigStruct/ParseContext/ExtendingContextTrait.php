789feb55bc303e162d867ce0202dc7e08e11a477
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-03 17:53:11 +0800

    Override the constructor

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/ExtendingContextTrait.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/ExtendingContextTrait.php
@@ -6,4 +6,12 @@ namespace Endermanbugzjfc\ConfigStruct\ParseContext;
 
 trait ExtendingContextTrait
 {
+
+    /**
+     * Please use {@link self::create()} instead.
+     */
+    private function __construct()
+    {
+    }
+
 }
