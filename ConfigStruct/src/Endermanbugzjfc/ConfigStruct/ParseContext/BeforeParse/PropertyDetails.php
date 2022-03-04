9eaa66346222399c2fbd3dfa50e1235b56615921
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-04 21:28:58 +0800

    Added PropertyDetails.php

---
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/BeforeParse/PropertyDetails.php
@@ -0,0 +1,15 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ConfigStruct\ParseContext\BeforeParse;
+
+final class PropertyDetails
+{
+
+    public function __construct()
+    {
+    }
+
+}
