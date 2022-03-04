8af08490b394502e3247603d63a1efcd9a9e6158
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-04 21:30:01 +0800

    Implemented PropertyDetails.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/BeforeParse/PropertyDetails.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseContext/BeforeParse/PropertyDetails.php
@@ -5,11 +5,32 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ConfigStruct\ParseContext\BeforeParse;
 
+use ReflectionProperty;
+
 final class PropertyDetails
 {
 
-    public function __construct()
+    public function __construct(
+        protected ReflectionProperty $reflection,
+        protected string $keyName
+    )
     {
+    }
+
+    /**
+     * @return ReflectionProperty
+     */
+    public function getReflection() : ReflectionProperty
+    {
+        return $this->reflection;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKeyName() : string
+    {
+        return $this->keyName;
     }
 
 }
