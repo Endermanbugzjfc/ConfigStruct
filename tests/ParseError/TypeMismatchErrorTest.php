428ee9d3c1aaa2d3d5fa0496c1193ad04feb26bb
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-06 02:59:48 +0800

    Added TypeMismatchErrorTest.php

---
+++ tests/ParseError/TypeMismatchErrorTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ConfigStruct\ParseError;
+
+use PHPUnit\Framework\TestCase;
+
+class TypeMismatchErrorTest extends TestCase
+{
+
+    public function testGetMessage()
+    {
+
+    }
+
+    public function testGetExpectedTypes()
+    {
+
+    }
+}
