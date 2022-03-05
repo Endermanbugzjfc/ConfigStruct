8a72411b32758b7cee44efb0310787cb368a71ad
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-05 20:12:50 +0800

    Documented BaseParseError.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseError/BaseParseError.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseError/BaseParseError.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ConfigStruct\ParseError;
 
+use Endermanbugzjfc\ConfigStruct\ParseError;
 use Stringable;
 use Throwable;
 
+/**
+ * A different error system from PHP {@link Throwable} which is used in parse errors tree. And is wrapped in a {@link ParseError} once being thrown.
+ */
 abstract class BaseParseError implements Stringable
 {
 
