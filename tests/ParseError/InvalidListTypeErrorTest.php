4897b726187fa9679da00dcc00088757b45a9889
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-06 02:49:23 +0800

    Declare strict type in test files

--- tests/ParseError/InvalidListTypeErrorTest.php
+++ tests/ParseError/InvalidListTypeErrorTest.php
@@ -1,5 +1,8 @@
 <?php
 
+
+declare(strict_types=1);
+
 namespace Endermanbugzjfc\ConfigStruct\ParseError;
 
 use Endermanbugzjfc\ConfigStruct\ListType;
--- tests/Utils/ConfigStructUtilsTest.php
+++ tests/Utils/ConfigStructUtilsTest.php
@@ -1,5 +1,8 @@
 <?php
 
+
+declare(strict_types=1);
+
 namespace Endermanbugzjfc\ConfigStruct\Utils;
 
 use PHPUnit\Framework\TestCase;
