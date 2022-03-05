aee2b8c694908b39f933537052b4b05f680be231
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-06 02:36:47 +0800

    Added InvalidListTypeErrorTest.php

---
+++ tests/ParseError/InvalidListTypeErrorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct\ParseError;
+
+use PHPUnit\Framework\TestCase;
+
+class InvalidListTypeErrorTest extends TestCase
+{
+
+    public function testGetMessage()
+    {
+
+    }
+}
