a4567f533b1c5d3926a0dad2ccb1c335002a7061
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-05 20:15:09 +0800

    Mention invalid list type error in the document of ListType.php

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ListType.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ListType.php
@@ -3,6 +3,7 @@
 namespace Endermanbugzjfc\ConfigStruct;
 
 use Attribute;
+use Endermanbugzjfc\ConfigStruct\ParseError\InvalidListTypeError;
 
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 class ListType
@@ -11,7 +12,7 @@ class ListType
     /**
      * Struct candidate for elements in a list.
      *
-     * If multiple candidates are provided, only the one with least unhandled elements will be used. Incompatible structs will never be used. If there is no suitable struct for an element, that element will not be parsed, raw values will be returned.
+     * If multiple candidates are provided, only the one with least unhandled elements will be used. Incompatible structs will never be used. If there is no suitable struct for an element, that element will not be parsed, raw values will be returned. An {@link InvalidListTypeError} will be added to the errors tree during parse time.
      * @param string $type Class name of the struct candidate. If the class had become invalid during runtime, this candidate will be omitted.
      */
     public function __construct(
