770cbc47253a47c1a5ace84783d8636ba92b36f2
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-05 20:03:57 +0800

    Added getPrevious()

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseError/BaseParseError.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseError/BaseParseError.php
@@ -5,9 +5,15 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ConfigStruct\ParseError;
 
+use Throwable;
+
 abstract class BaseParseError
 {
 
     abstract public function getMessage();
+
+    public function getPrevious() : ?Throwable {
+        return null;
+    }
 
 }
