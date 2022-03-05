cc33e4867a63a47d577b462af12a0c512f0da83f
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-06 02:40:13 +0800

    Added ConfigStructUtilsTest.php

---
+++ tests/Utils/ConfigStructUtilsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Endermanbugzjfc\ConfigStruct\Utils;
+
+use PHPUnit\Framework\TestCase;
+
+class ConfigStructUtilsTest extends TestCase
+{
+
+    public function testArrayIsList()
+    {
+
+    }
+}
