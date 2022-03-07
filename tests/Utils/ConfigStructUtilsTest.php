e0c46670ffd191144ba90d77d7fbc680407354dd
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-07 12:05:36 +0800

    Added test

--- tests/Utils/ConfigStructUtilsTest.php
+++ tests/Utils/ConfigStructUtilsTest.php
@@ -34,4 +34,105 @@ class ConfigStructUtilsTest extends TestCase
             )
         );
     }
+
+    public function testArrayUnsetRecursive()
+    {
+        $array = [
+            "testAssociativeArray" => [
+                "a" => "testUnset",
+                "b" => "testChange",
+                "c" => "testOmit"
+            ],
+            "testList" => [
+                "testUnset",
+                "testChange",
+                "testOmit"
+            ],
+            2 => "testUnset",
+            3 => "testChange",
+            4 => "testOmit"
+        ];
+
+        ConfigStructUtils::arrayUnsetRecursive(
+            $array,
+            function (
+                array $keys,
+                mixed &$value
+            ) : bool {
+                if ($value === "testUnset") {
+                    $this->assertTrue(
+                        $keys === [
+                            "testAssociativeArray",
+                            "a"
+                        ]
+                        or
+                        $keys === [
+                            "testList",
+                            0
+                        ]
+                        or
+                        $keys === [
+                            2
+                        ]
+                    );
+
+                    return true;
+                }
+                if ($value === "testChange") {
+                    $this->assertTrue(
+                        $keys === [
+                            "testAssociativeArray",
+                            "b"
+                        ]
+                        or
+                        $keys === [
+                            "testList",
+                            1
+                        ]
+                        or
+                        $keys === [
+                            3
+                        ]
+                    );
+
+                    $value = "changed";
+                }
+                if ($value === "testOmit") {
+                    $this->assertTrue(
+                        $keys === [
+                            "testAssociativeArray",
+                            "c"
+                        ]
+                        or
+                        $keys === [
+                            "testList",
+                            2
+                        ]
+                        or
+                        $keys === [
+                            4
+                        ]
+                    );
+                }
+
+                return false;
+            }
+        );
+
+        $this->assertTrue(
+            $array === [
+                "testAssociativeArray" => [
+                    "b" => "changed",
+                    "c" => "testOmit"
+                ],
+                "testList" => [
+                    "changed",
+                    "testOmit"
+                ],
+                3 => "changed",
+                4 => "testOmit"
+            ]
+        );
+    }
+
 }
