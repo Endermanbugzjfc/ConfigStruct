e1a7e12fa99df68dc9a9a83c3c9069b23541857a
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-09 12:27:03 +0800

    Added some dummy objects

---
+++ tests/Dummy/Extending/A.php
@@ -0,0 +1,11 @@
+<?php
+
+
+declare(strict_types=1);
+
+class A extends Base
+{
+
+    public mixed $a;
+
+}
---
+++ tests/Dummy/Extending/B.php
@@ -0,0 +1,11 @@
+<?php
+
+
+declare(strict_types=1);
+
+class B extends Base
+{
+
+    public mixed $b;
+
+}
