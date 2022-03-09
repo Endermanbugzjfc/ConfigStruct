90432d54ea8d7e981168fab754f8827eeaef70f8
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-09 18:02:26 +0800

    Added KeyNameTest.php

---
+++ KeyNameTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ConfigStruct;
+
+use PHPUnit\Framework\TestCase;
+
+class KeyNameTest extends TestCase
+{
+
+    public function test__construct()
+    {
+
+    }
+}
