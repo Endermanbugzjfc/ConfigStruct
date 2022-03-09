9412836ecb9df0330faf9505701d9a0a8c465af3
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-09 18:11:59 +0800

    Implemented KeyNameTest.php

--- KeyNameTest.php
+++ KeyNameTest.php
@@ -9,8 +9,54 @@ use PHPUnit\Framework\TestCase;
 class KeyNameTest extends TestCase
 {
 
+    /**
+     * @throws ParseErrorsWrapper
+     */
     public function test__construct()
     {
+        // TODO: Test for emit.
+        $object = new class () {
 
+            #[KeyName("a")]
+            public mixed $testOneKeyName;
+
+            #[KeyName("b")] #[KeyName("c")]
+            public mixed $testMultipleKeyNames;
+
+        };
+        $context = Parse::object(
+            [
+                "a" => "a",
+                "b" => "b",
+                "c" => "c"
+            ],
+            $object
+        );
+        $context->copyToObject(
+            $object,
+            "root object"
+        );
+
+        $this->assertTrue(
+            $object->testOneKeyName === "a"
+        );
+        $this->assertTrue(
+            $object->testMultipleKeyNames === "b"
+        );
+
+        $context = Parse::object(
+            [
+                "c" => "c"
+            ],
+            $object
+        );
+        $context->copyToObject(
+            $object,
+            "root object"
+        );
+
+        $this->assertTrue(
+            $object->testMultipleKeyNames === "c"
+        );
     }
 }
