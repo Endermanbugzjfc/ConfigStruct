36b58f55b96a1f1e3efd18a1961f8bd4ead6bb6b
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-09 12:46:59 +0800

    Added namespaces

--- tests/Dummy/Extending/A.php
+++ tests/Dummy/Extending/A.php
@@ -3,6 +3,8 @@
 
 declare(strict_types=1);
 
+namespace Endermanbugzjfc\ConfigStruct\Dummy\Extending;
+
 final class A extends Extendable
 {
 
--- tests/Dummy/Extending/B.php
+++ tests/Dummy/Extending/B.php
@@ -3,6 +3,8 @@
 
 declare(strict_types=1);
 
+namespace Endermanbugzjfc\ConfigStruct\Dummy\Extending;
+
 final class B extends Extendable
 {
 
