333518803106429f9a651755f80f59ef57dde468
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-10 20:48:28 +0800

    Added ParseErrorsWrapperTest.php

---
+++ tests/ParseErrorsWrapperTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ConfigStruct;
+
+use PHPUnit\Framework\TestCase;
+
+class ParseErrorsWrapperTest extends TestCase
+{
+
+    public function testGetErrorFilter()
+    {
+
+    }
+
+    public function testRegenerateErrorMessage()
+    {
+
+    }
+
+    public function testGetIndentation()
+    {
+
+    }
+}
