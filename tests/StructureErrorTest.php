096bcf011f5bf0416e0eb407376846fb9e5f249d
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-11 00:53:03 +0800

    Added StructureErrorTest.php

---
+++ tests/StructureErrorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ConfigStruct;
+
+use PHPUnit\Framework\TestCase;
+
+class StructureErrorTest extends TestCase
+{
+
+    public function test__constructDuplicatedKeyNames()
+    {
+
+    }
+
+    public function test__constructDuplicatedListTypes()
+    {
+
+    }
+
+    public function test__constructInvalidListTypes()
+    {
+
+    }
+
+    public function test__constructObjectConstructorProtected()
+    {
+
+    }
+
+    public function test__constructObjectConstructorWithArguments()
+    {
+
+    }
+
+}
