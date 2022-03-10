3860fa4f3ea8c44af5f59bb427d8721401f28262
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-10 21:13:36 +0800

    Implemented parseErrorsWrapperProvider()

--- tests/ParseErrorsWrapperTest.php
+++ tests/ParseErrorsWrapperTest.php
@@ -4,10 +4,41 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ConfigStruct;
 
+use Endermanbugzjfc\ConfigStruct\Dummy\Extending\A;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 class ParseErrorsWrapperTest extends TestCase
 {
+
+    private static function parseErrorsWrapperProvider() : ParseErrorsWrapper
+    {
+        $object = new class() {
+
+            #[ListType(A::class)]
+            public $testIndentationWithList;
+
+        };
+        $context = Parse::object(
+            [
+                "testIndentationWithList" => [
+                    "a" => null
+                ]
+            ],
+            $object
+        );
+        try {
+            $context->copyToObject(
+                $object,
+                "root object"
+            );
+        } catch (ParseErrorsWrapper $err) {
+            return $err;
+        }
+        throw new RuntimeException(
+            "No errors when copy parsed data to object"
+        );
+    }
 
     public function testGetErrorFilter()
     {
