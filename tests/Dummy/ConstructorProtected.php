87c5d30b54d1feafda83fbb681d063e51271c896
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-11 00:54:11 +0800

    Implemented ConstructorProtected.php

---
+++ tests/Dummy/ConstructorProtected.php
@@ -0,0 +1,15 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ConfigStruct\Dummy;
+
+final class ConstructorProtected
+{
+
+    protected function __construct()
+    {
+    }
+
+}
