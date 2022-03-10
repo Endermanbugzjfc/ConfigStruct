94407ba7e010c5ff4a0d466fa2fa7f3e24a5187a
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-11 01:30:11 +0800

    Implemented expectPreviousExceptionMessage()

--- tests/StructureErrorTest.php
+++ tests/StructureErrorTest.php
@@ -4,10 +4,49 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ConfigStruct;
 
+use AssertionError;
 use PHPUnit\Framework\TestCase;
 
 class StructureErrorTest extends TestCase
 {
+
+    /**
+     * @throws ParseErrorsWrapper
+     */
+    private function expectPreviousExceptionMessage(
+        string $message,
+        object $object,
+        ?string $property
+    ) : void
+    {
+        try {
+            $context = Parse::object(
+                [],
+                $object
+            );
+            $context->copyToObject(
+                $object,
+                "root object"
+            );
+        } catch (StructureError $err) {
+            $class = $object::class;
+            $expected = "Invalid structure in $class";
+            if ($property !== null) {
+                $expected .= "->$property";
+            }
+
+            $this->assertTrue(
+                $err->getMessage() === $expected
+            );
+            $this->assertTrue(
+                $err->getPrevious()->getMessage() === $message
+            );
+            return;
+        }
+        throw new AssertionError(
+            "No " . StructureError::class . " had been thrown"
+        );
+    }
 
     public function test__constructDuplicatedKeyNames()
     {
