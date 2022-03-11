921a7caf9b08a7c2da3b457f06234b0d875c188c
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-03-11 20:57:33 +0800

    Added ChildObjectContextTest.php

---
+++ tests/ParseContext/ChildObjectContextTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ConfigStruct\ParseContext;
+
+use PHPUnit\Framework\TestCase;
+
+class ChildObjectContextTest extends TestCase
+{
+
+    public function testGetUnhandledElements()
+    {
+
+    }
+}
