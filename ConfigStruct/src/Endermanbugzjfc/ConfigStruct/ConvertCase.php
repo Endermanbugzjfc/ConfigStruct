65202ff38c6cd76a9a5a34401ce618dd59ca8263
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-17 09:04:05 +0800

    Added lowercase and uppercase

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ConvertCase.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ConvertCase.php
@@ -3,6 +3,8 @@
 namespace Endermanbugzjfc\ConfigStruct;
 
 use Attribute;
+use function strtolower;
+use function strtoupper;
 
 /**
  * Walk selected key names in a class with the case converter when emitting or parsing.
@@ -11,10 +13,35 @@ use Attribute;
 class ConvertCase
 {
 
+    public const LOWERCASE = [self::class, "lowercase"];
+    public const UPPERCASE = [self::class, "uppercase"];
+
     public function __construct(
         callable $caseConverter
     )
     {
+    }
+
+    /**
+     * lowercase.
+     * @param string $name
+     * @return string
+     */
+    public function lowercase(
+        string $name
+    ) : string {
+        return strtolower($name);
+    }
+
+    /**
+     * UPPERCASE.
+     * @param string $name
+     * @return string
+     */
+    public function uppercase(
+        string $name
+    ) : string {
+        return strtoupper($name);
     }
 
 }
