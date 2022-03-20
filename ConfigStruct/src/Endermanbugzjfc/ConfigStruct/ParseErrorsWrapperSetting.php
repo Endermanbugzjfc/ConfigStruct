9439dc536abf47f5388102cbfef2a7c67381ad87
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-20 15:43:32 +0800

    Added errorFilter()

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseErrorsWrapperSetting.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseErrorsWrapperSetting.php
@@ -5,7 +5,22 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ConfigStruct;
 
+use Endermanbugzjfc\ConfigStruct\ParseError\BaseParseError;
+
 class ParseErrorsWrapperSetting
 {
+
+    /**
+     * @param array $keys A list of error labels that can be used for identifying which error in the tree has just been walked.
+     * @param BaseParseError $parseError
+     * @return bool False = the error will not be displayed in the final error message.
+     */
+    public function errorFilter(
+        array          $keys,
+        BaseParseError $parseError
+    ) : bool
+    {
+        return true;
+    }
 
 }
