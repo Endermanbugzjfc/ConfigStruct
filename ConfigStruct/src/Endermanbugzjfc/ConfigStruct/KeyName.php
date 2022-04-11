3a34996464c5be6658659d1897338a22c760e1bf
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-04-12 01:20:33 +0800

    Allow reserving the text case of a key name

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/KeyName.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/KeyName.php
@@ -6,16 +6,20 @@ namespace Endermanbugzjfc\ConfigStruct;
 
 use Attribute;
 
-/**
- * Emit and parse this property with the name provided in the attribute instead of the property name.
- *
- * If multiple names are provided, only the first one is used for emitting, and the first name available in the input (the first key that exists in the parse data) is used for parsing. This gets useful when updating configs.
- */
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 class KeyName
 {
+
+    /**
+     * Emit and parse this property with the name provided in the attribute instead of the property name.
+     *
+     * If multiple names are provided, only the first one is used for emitting, and the first name available in the input (the first key that exists in the parse data) is used for parsing. This gets useful when updating configs.
+     *
+     * @param bool $reserveTextCase True = this key name will not be affected by the {@link ConvertCase} attribute.
+     */
     public function __construct(
         int|string $name,
+        bool $reserveTextCase = false, // TODO: Add test.
     ) {
     }
 }
