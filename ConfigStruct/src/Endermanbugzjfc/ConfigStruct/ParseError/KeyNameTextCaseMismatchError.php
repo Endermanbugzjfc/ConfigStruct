0f52130e9deb1b43a6e774a4cceb4bb8f6766b80
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-04-12 01:54:35 +0800

    Added key name text case mismatch parse error

---
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/ParseError/KeyNameTextCaseMismatchError.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ConfigStruct\ParseError;
+
+use function implode;
+
+/**
+ * Parse time error. When an element has one or more corresponding properties but its key name has the wrong text case.
+ */
+final class KeyNameTextCaseMismatchError extends BaseParseError
+{
+
+    /**
+     * @param string[] $guessNames
+     * @param string $inputName
+     */
+    public function __construct(
+        protected array  $guessNames,
+        protected string $inputName
+    ) {
+        parent::__construct(null);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getGuessNames() : array
+    {
+        return $this->guessNames;
+    }
+
+
+    public function getInputName() : string
+    {
+        return $this->inputName;
+    }
+
+    public function getMessage() : string
+    {
+        $guessNames = implode(
+            "\" / \"",
+            $this->getGuessNames()
+        );
+        return "Element key name \"{$this->getInputName()}\" has the wrong text case, guess it is supposed to \"$guessNames\"";
+    }
+}
