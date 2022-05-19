aae6871437fb04518f288c853b5db3dab7308c07
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-05-19 21:22:42 +0800

    Return struct parse context

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/StructTrait.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/StructTrait.php
@@ -6,15 +6,20 @@ namespace Endermanbugzjfc\ConfigStruct;
 
 use ReflectionClass;
 use ReflectionProperty;
+use TypeError;
 use function array_key_exists;
 use function array_unshift;
 
 trait StructTrait
 {
-    public static function parse(array $data) : self
+    /**
+     * @return StructParseContext<self>
+     */
+    public static function parse(array $data) : StructParseContext
     {
         $self = new self();
-        $class = new ReflectionClass(self::class);
+        $class = new ReflectionClass($self);
+        $context = new StructParseContext($class, $self);
         $isPublic = ReflectionProperty::IS_PUBLIC;
         $properties = $class->getProperties($isPublic);
 
@@ -23,15 +28,24 @@ trait StructTrait
             $rename = Rename::get($property);
             $names = $rename->moreNames;
             array_unshift($names, $rename->name);
-            $ok = false;
+            $exists = false;
             foreach ($names as $name) {
                 if (array_key_exists($name, $data)) {
-                    $self->$propertyName = $data[$name];
-                    $ok = true;
+                    $typeError = null;
+                    try {
+                        $self->$propertyName = $data[$name];
+                    } catch (TypeError $typeError) {
+                    }
+                    $exists = true;
                 }
             }
+
+            $context->properties[$propertyName] = $property;
+            $context->presences[$propertyName] = $exists;
+            $context->typeErrors[$propertyName] = $typeError;
         }
 
-        return $self;
+        $context->finalize();
+        return $context;
     }
 }
