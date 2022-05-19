ad4826a466cee23ab31b27fa28c43b6ece2aa908
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-05-19 19:54:31 +0800

    Added struct trait (unimplemented)

---
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/StructTrait.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ConfigStruct;
+
+trait StructTrait
+{
+    public static function parse(array $data) : self
+    {
+        // TODO: struct trait.
+    }
+}
