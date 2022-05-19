8c4f8301a9faf0fe137cbe986a86f257c9b40ba1
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-05-19 20:39:43 +0800

    Parser prototype

--- ConfigStruct/src/Endermanbugzjfc/ConfigStruct/StructTrait.php
+++ ConfigStruct/src/Endermanbugzjfc/ConfigStruct/StructTrait.php
@@ -4,10 +4,34 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ConfigStruct;
 
+use ReflectionClass;
+use ReflectionProperty;
+use function array_key_exists;
+use function array_unshift;
+
 trait StructTrait
 {
     public static function parse(array $data) : self
     {
-        // TODO: struct trait.
+        $self = new self();
+        $class = new ReflectionClass(self::class);
+        $isPublic = ReflectionProperty::IS_PUBLIC;
+        $properties = $class->getProperties($isPublic);
+
+        foreach ($properties as $property) {
+            $propertyName = $property->getName();
+            $rename = Rename::get($property);
+            $names = $rename->moreNames;
+            array_unshift($names, $rename->name);
+            $ok = false;
+            foreach ($names as $name) {
+                if (array_key_exists($name, $data)) {
+                    $self->$propertyName = $data[$name];
+                    $ok = true;
+                }
+            }
+        }
+
+        return $self;
     }
 }
